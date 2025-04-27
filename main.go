71020dc3d85680689647115c158876309ed1a2f2
stenioignacio-jacto <stenioignacio@gmail.com>
2025-04-27 12:46:24 -0300

    Adicionando arquivos iniciais projeto

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.RawQuery)
+	fmt.Fprintf(w, `
+          ##         .
+    ## ## ##        ==
+ ## ## ## ## ##    ===
+/"""""""""""""""""\___/ ===
+{                       /  ===-
+\______ O           __/
+ \    \         __/
+  \____\_______/
+
+	
+Hello from Aulao v1! :)
+
+`)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
